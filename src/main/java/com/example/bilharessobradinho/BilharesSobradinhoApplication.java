cdbf6f3f3bb13ed3e7de2a0de18c3ea3a8b2529c
rodrigovsousa <rodrigovsousa92@gmail.com>
2023-07-07 18:32:54 -0300

    Initial commit - Base project

---
+++ src/main/java/com/example/bilharessobradinho/BilharesSobradinhoApplication.java
@@ -0,0 +1,14 @@
+package com.example.bilharessobradinho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+public class BilharesSobradinhoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BilharesSobradinhoApplication.class, args);
+    }
+
+}
