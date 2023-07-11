044f73a537157ba6e99a5a65241e2ba9d850a392
Julianna Souto <julisouto@yahoo.com.br>
2023-07-11 19:16:54 -0300

    Correções de @table, indentação e camel case

--- src/main/java/com/example/bilharessobradinho/domain/Product.java
+++ src/main/java/com/example/bilharessobradinho/domain/Product.java
@@ -1,16 +1,19 @@
 package com.example.bilharessobradinho.domain;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 
 import java.util.Objects;
 
 @Entity
+@Table(name = "product")
+
 public class Product {
 
-    private String Serial_Number;
-    private String Type;
-    private String Name;
-    private String Amount;
+    private String serial_Number;
+    private String type;
+    private String name;
+    private String amount;
 
     public String getSerial_Number() {
         return Serial_Number;
@@ -49,7 +52,10 @@ public class Product {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof Product product)) return false;
-        return getSerial_Number().equals(product.getSerial_Number()) && getType().equals(product.getType()) && getName().equals(product.getName()) && getAmount().equals(product.getAmount());
+        return getSerial_Number().equals(product.getSerial_Number()) &&
+               getType().equals(product.getType()) && getName().equals(product.getName()) &&
+               getAmount().equals(product.getAmount());
+
     }
 
     @Override
